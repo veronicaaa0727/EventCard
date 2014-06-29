cee3f3fde35dfde0198a72a2d14a17c456976a09
Pengqi Liu <lpq1990@gmail.com>
2014-06-29 12:02:26 -0700

    Add Application EventCard

--- EventCard_Project/EventCard_Project/settings.py
+++ EventCard_Project/EventCard_Project/settings.py
@@ -29,15 +29,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south'
 )
+
+THIRD_PARTY_APPS = (
+    'south',
+)
+
+LOCAL_APPS = (
+    'EventCard',
+)
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
