e2238e6bb2cb748dbfa990db3eb0319f30635391
Matt Mieckowski <matt@mieckowski.com>
2014-08-08 21:32:56 -0700

    created new xcode project, added .gitignore file

---
+++ Clairvoyance/Clairvoyance/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clairvoyance
+//
+//  Created by Matt on 8/8/14.
+//  Copyright (c) 2014 Clairvoyance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
